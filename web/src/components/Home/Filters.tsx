1f2f74c140ccd710e983d0734e6018d5666b6d52
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-07-12 21:13:39 -0300

    feat: update filter component

--- web/src/components/Home/Filters.tsx
+++ web/src/components/Home/Filters.tsx
@@ -1,13 +1,44 @@
+"use client"
+import { api } from "@/app/lib/api"
+import { ChefHat } from "lucide-react"
+import {useMemo, useState} from "react"
+
+
+interface FoodType{
+    description: string
+}
+
 export default function Filters() {
+    const [filters, setFilters] = useState<FoodType[]>()
+
+    const memo = useMemo(async ()=>{
+        await api.get("/foodtypes")
+        .then(({data}) => setFilters(data))
+    }, [])
+
     return(
         <section>
             <div className="flex justify-center items-center gap-2">
-                <hr className="bg-gray-grayPrimary w-[40%] h-1"/>
-                <p className="text-orange-orangePrimary w-[20%] text-center">Filtros</p>
-                <hr className="bg-gray-grayPrimary w-[40%] h-1"/>
+                <hr className="bg-gray-grayPrimary w-[50%] h-1"/>
+                <p className="text-orange-orangePrimary  text-center text-lg">Filtros</p>
+                <hr className="bg-gray-grayPrimary w-[50%] h-1"/>
             </div>
-            <div>
-
+            <div className="flex gap-4 lg:gap-8 ml-2 mr-2 justify-center items-center">
+                {
+                    filters?.map((foodType, index)=>{
+                  
+                        return(
+                            <div 
+                            key={index.toString()}
+                            className="flex flex-col p-1 justify-center items-center group ">
+                                <button className="rounded-full shadow h-[37px] w-[38px] lg:h-14 lg:w-14 justify-center items-center flex group-hover:bg-orange-orangePrimary transition-all">
+                                    <ChefHat color="#cdcdcd" size={25} className="group-hover:fill-orange-orangePrimary transition-all" />
+                                </button>
+                                <p className="text-black text-center text-md group-hover:text-orange-orangePrimary">{foodType.description}</p>
+                            </div>
+                        )
+                    })
+                }
             </div>
             <hr className="bg-gray-grayPrimary w-[100%] h-1"/>
         </section>
--- web/src/components/Home/Search.tsx
+++ web/src/components/Home/Search.tsx
@@ -3,15 +3,14 @@ import { Search } from 'lucide-react';
 
 export default function SearchArea() {
     return(
-        <div className="flex group flex-col container mx-auto shadow min-h-[100px] lg:min-h-[150px] mt-5 items-center justify-center p-2 relative">
-          
+        <div className="flex group flex-col container mx-auto min-h-[100px] lg:min-h-[150px] mt-5 items-center justify-center p-2 relative">
             <h1 className="text-xl">Onde será a reserva de hoje?</h1>
             <div className="flex ">
                 <Input type="text" name="search"
                 className="border-t-2 border-l-2 border-b-2 rounded-tl-2xl rounded-bl-2xl lg:w-[25rem] outline-orange-orangePrimary p-1" />
                 <button className="flex w-14 lg:w-auto justify-center items-center gap-2 bg-orange-orangePrimary rounded-br-2xl rounded-tr-2xl p-1
                 transition-all hover:bg-orange-500" >
-                    <Search color="#ffff"/>
+                    <Search color="#ffff" />
                     <p className="lg:flex hidden text-white">Buscar</p>
                 </button>
             </div>
